988ae48cf138eb90c4de5662b1870d812a61c2ae
bentheone <rben5838@gmail.com>
2024-04-07 20:52:10 +0200

    Made Admin authentication and tried to make the search of buses pissible but not yet worked

--- admin/backend/index.js
+++ admin/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const mysql = require('mysql')
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -9,37 +10,55 @@ const SECRET_KEY = 'my-secret-key';
 
 app.use(bodyParser.json());
 
-const users = [
-    {
-        id: 1,
-        email: 'john@gmail.com',
-        name:'Jogn Doe',
-        passwordHash: '$2b$10$Nalv4t8ixK1M4ti7O6H7Qet/hdF3.Lm6sjbMeIz4bB3b4m7KP8RVO',
-        agency: 'Ritco travels'
-    },
-    {
-        id: 2,
-        email: 'jane@gmail.com',
-        name: 'Jane Doe',
-        passwordHash: '$2b$10$Nalv4t8ixK1M4ti7O6H7Qet/hdF3.Lm6sjbMeIz4bB3b4m7KP8RVO',
-        agency: 'Kigali Coach'
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "katisha_system_db"
+})
+conn.connect(err=>{
+    if (err){
+    console.error("Error connecting to the database", err)
+    return;
     }
-]
+    console.log("Connected")
+});
 
-app.post('/login',(req,res)=>{
+app.post('/', (req,res) => {
     const { email, password } = req.body;
-    const user = users.find(user => user.email === email);
-    if(!user){
-        return res.status(404).json({Error:"User not found"})
-    }
-    bcrypt.compare(password, user.passwordHash, (err, result) => {
-        if(err || !result) {
-            return res.status(401).json({Error:"Invalid username or password"});
+    const sql = "SELECT * FROM admins WHERE email = ?"
+    conn.query(sql, [email], (err, results) => {
+        if(err) {
+            console.error("Error fetching user:", err)
         }
-        const token = jwt.sign({id: user.id, name: user.name, agency: user.agency}, SECRET_KEY);
-        res.json({ token });
+        if(results.length === 0) {
+            return res.status(500).json({ error: 'User not found'})
+        }
+        const user = results[0];
+
+        bcrypt.compare(password, user.passwordHash, (err, result) => {
+            if(err || !result) {
+                return res.status(401).json({error: "Password incorrect"})
+            }
+            const agencyQuery = "SELECT * FROM agencies WHERE AgencyID = ?"
+            conn.query(agencyQuery, [user.agencyid], (err, agencyResults) => {
+                if(err) {
+                    console.error("Error fetching agency", err)
+                    return res.status(500).json({ error: "Internal Server Error"});
+
+                }
+                if(agencyResults.length === 0) {
+                    return res.status(500).json({error: "Agency Not found"})
+                }
+
+                const token = jwt.sign({id: user.id, name: user.name, agency: agencyResults[0].AgencyName}, SECRET_KEY)
+                res.json({ token })
+            })
+        })
     })
 })
+
+
 
 
 app.get('/login', (req, res) => {
--- user/backend/index.js
+++ user/backend/index.js
@@ -1,1 +1,68 @@
-// Local user backend in node will be done here...
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'katisha_system_db'
+});
+
+conn.connect(err => {
+    if(err){
+    console.error('Error connecting to mysql');
+    return;
+    }
+    console.log('Connected')
+})
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/locations',(req, res) => {
+    const query = req.query.q;
+    const sql = "SELECT * FROM locations WHERE location_name LIKE ?";
+    conn.query(sql, [`%${query}%`], (err,result) => {
+        if(err) {
+            console.error('Error executing query:', err)
+            res.status(500).json({error: "Internal Server Error"});
+            return;
+        }
+        res.json(result);
+
+
+    })
+
+});
+app.post('/search', (req,res)=>{
+    const {from, to} = req.body;
+    const currentTime = new Date();
+    const minDeparture = new Date(currentTime.getTime() + 5 * 60000);
+    const sql = `
+    SELECT b.*, d.DestinationName
+    FROM buses b
+    INNER JOIN destinations d ON b.DestinationID = d.DestinationID
+    WHERE d.DestinationName = ? AND b.DeptTime >= ? AND b.AvailableSeats > 0
+    `;
+    conn.query(sql, [`${from}-${to}`, minDeparture], (err, results) => {
+       if(err) {
+           console.error("Error:", err)
+           return res.status(500).json({error: "Internal Server Error"})
+       }
+       res.json({ from, to, data: results });
+    });
+});
+app.get('/bus-details', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/bus-details.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port  http://localhost${PORT}`);
+})
